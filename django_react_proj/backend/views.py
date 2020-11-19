afba9f00a2e182b413a34a08ab2f53965324c97d
Dawitworku <dwc99519@marymount.edu>
2020-11-19 13:34:20 -0500

    Added Serializer.py, added url for the api's and finally created an api for company and department

--- django_react_proj/backend/views.py
+++ django_react_proj/backend/views.py
@@ -1,6 +1,17 @@
 from django.shortcuts import render, redirect, HttpResponse
+from rest_framework import viewsets
+from .serializers import *
+from .models import *
 
 # Create your views here.
 
 def index(request):
     return HttpResponse("Checking")
+
+class DepartmentView(viewsets.ModelViewSet):
+    serializer_class = DepartmentSerializer
+    queryset = Department.objects.all()
+
+class CompanyView(viewsets.ModelViewSet):
+    serializer_class = CompanySerializer
+    queryset = Company.objects.all()
