bb2dc1991a097cece42e2ea032c441f917b23254
John Clark <clark2j2@johnmclark.com>
2020-11-30 22:44:19 -0500

    Updates to Profile Page

--- django_react_proj/frontend/client/src/components/profile/ProfileMain.js
+++ django_react_proj/frontend/client/src/components/profile/ProfileMain.js
@@ -1,19 +1,35 @@
 import React from "react";
 import ProfileInfo from './ProfileInfo';
 import LogoutButton from '../welcome-auth/LogoutButton';
+import Navbar from 'react-bootstrap/Navbar'
+import Nav from 'react-bootstrap/Nav'
 import { Link } from '@reach/router';
+import { useAuth0 } from "@auth0/auth0-react";
 
 const ProfileMain = () => {
 
+    const { logout, user, isAuthenticated } = useAuth0();
     return (
-        <div>
-            <h1>Welcome to Job Boards Home!</h1>
-            <ProfileInfo />
-            <Link to="/company">Company Home Page</Link>
+        isAuthenticated && (
             <div>
-                <LogoutButton />
+                <Navbar bg="primary" variant="dark">
+                    <Navbar.Brand>Welcome to Job Boards!</Navbar.Brand>
+                    <Nav className="mr-auto">
+                        <Nav.Link href="/company">Company Page</Nav.Link>
+                        <Nav.Link href="#features">Placeholder</Nav.Link>
+                        <Nav.Link href="#pricing">Placeholder</Nav.Link>
+                    </Nav>
+                    <Nav>
+                        <Nav.Link onClick={() => logout()}>Hello, {user.nickname}! Click here to Log Out!</Nav.Link>
+                        &nbsp;
+                        <Navbar.Text><img src={user.picture} alt={user.name} height="45px" /></Navbar.Text>
+                    </Nav>
+                </Navbar>
+                {/* <div>
+                    <LogoutButton />
+                </div> */}
             </div>
-        </div>
+        )
     )
 }
 
